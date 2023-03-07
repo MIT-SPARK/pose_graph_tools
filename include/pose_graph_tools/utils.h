6012c1320cdf27aeaeee373d23f47bb3f53dd33e
Nathan Hughes <nathan.h.hughes@gmail.com>
2023-03-07 11:12:03 -0500

    Feature/kimera multi (#12) (#7)
    * transmit bearing vectors and depth instead of 3D keypoints
    
    * add ROS message for BoW requests
    
    * change depth in VLC message to float
    
    * specify source and dest robot in kimera-multi front-end messages
    
    * add new type and msgs to support pose graph query
    
    * add custom msgs for loop closure sync
    
    * add cv_bridge dependency
    
    * remove dbow2 dependency
    
    * drop cv_bridge dependency
    
    Co-authored-by: yunchang <yunchang@mit.edu>
    Co-authored-by: Yulun Tian <yulun@mit.edu>

--- include/pose_graph_tools/utils.h
+++ include/pose_graph_tools/utils.h
@@ -9,11 +9,12 @@
 #include <pose_graph_tools/PoseGraph.h>
 
 namespace pose_graph_tools {
-bool savePoseGraphEdgesToFile(const PoseGraph& graph,
-                              const std::string& filename);
+bool savePoseGraphEdgesToFile(const PoseGraph &graph,
+                              const std::string &filename);
 
 // Filter duplicate edges in the input pose graph
-// Two edges are considered duplicate if they share the common key_from, key_to, robot_from, robot_to
-PoseGraph filterDuplicateEdges(const PoseGraph& graph_in);
+// Two edges are considered duplicate if they share the common key_from, key_to,
+// robot_from, robot_to
+PoseGraph filterDuplicateEdges(const PoseGraph &graph_in);
 
-}  // namespace pose_graph_tools
+} // namespace pose_graph_tools
---
+++ src/types.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright Notes
+ *
+ * Authors: Yun Chang (yunchang@mit.edu)
+ */
+
+#include <pose_graph_tools/types.h>
+
+namespace pose_graph_tools {} // namespace pose_graph_tools
