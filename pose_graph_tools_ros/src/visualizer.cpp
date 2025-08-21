2993241e8ebeef12f5a9137d4f20b21079d60dd7
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-08-21 15:56:15 -0400

    add parameters and simplify visualizer marker drawing

--- pose_graph_tools_ros/src/visualizer.cpp
+++ pose_graph_tools_ros/src/visualizer.cpp
@@ -39,11 +39,20 @@ class Visualizer : public rclcpp::Node {
 
  private:
   // state
-  std::string frame_id_;
+  std_msgs::msg::Header last_header_;
   std::vector<Edge> odometry_edges_;
   std::vector<Edge> loop_edges_;
   std::vector<Edge> rejected_loop_edges_;
+  std::vector<Edge> other_edges_;
   KeyedPoses keyed_poses_;
+  // params
+  bool show_heading_;
+  double odom_scale_;
+  double loop_closure_scale_;
+  double rejected_loop_closure_scale_;
+  double other_edge_scale_;
+  double node_scale_;
+  double text_scale_;
   // ros infrastructure
   rclcpp::Subscription<PoseGraph>::SharedPtr sub_;
   rclcpp::Publisher<MarkerArray>::SharedPtr pub_;
@@ -78,13 +87,13 @@ Quaternion orientationFromKey(const KeyedPoses& keyed_poses,
 }
 
 void makeMenuMarker(interactive_markers::InteractiveMarkerServer& server,
-                    const std::string& frame_id,
+                    const std_msgs::msg::Header& header,
                     const Pose& position,
                     const std::string& id_number) {
   interactive_markers::MenuHandler menu_handler;
 
   InteractiveMarker int_marker;
-  int_marker.header.frame_id = frame_id;
+  int_marker.header = header;
   int_marker.scale = 1.0;
   int_marker.pose = position;
   int_marker.name = id_number;
@@ -112,41 +121,39 @@ void makeMenuMarker(interactive_markers::InteractiveMarkerServer& server,
   menu_handler.apply(server, int_marker.name);
 }
 
-Marker build_odom_marker(const KeyedPoses& keyed_poses,
-                         const VisualizerEdges& odom_edges,
-                         const std::string& frame_id) {
+Marker build_edge_marker(
+    const KeyedPoses& keyed_poses,
+    const VisualizerEdges& edges,
+    const std::string& ns,
+    const std::function<std_msgs::msg::ColorRGBA(size_t)>& cmap,
+    double scale) {
   Marker marker;
-  marker.header.frame_id = frame_id;
-  marker.ns = "odom_edges";
+  marker.ns = ns;
   marker.id = 0;
   marker.action = Marker::ADD;
   marker.type = Marker::LINE_LIST;
-  for (size_t ii = 0; ii < odom_edges.size(); ++ii) {
-    int robot_id = odom_edges[ii].first.first;
-    const auto key1 = odom_edges[ii].first.second;
-    const auto key2 = odom_edges[ii].second.second;
+  marker.scale.x = scale;
+  marker.pose.orientation.w = 1.0;
 
-    marker.scale.x = 0.02;
-    marker.pose.orientation.w = 1.0;
-
-    marker.points.push_back(positionFromKey(keyed_poses, robot_id, key1));
-    marker.points.push_back(positionFromKey(keyed_poses, robot_id, key2));
-    marker.colors.push_back(makeColor(robot_id));
-    marker.colors.push_back(makeColor(robot_id));
+  for (const auto& [from, to] : edges) {
+    const auto& [from_robot, from_key] = from;
+    const auto& [to_robot, to_key] = to;
+    marker.points.push_back(positionFromKey(keyed_poses, from_robot, from_key));
+    marker.points.push_back(positionFromKey(keyed_poses, to_robot, to_key));
+    marker.colors.push_back(cmap(from_robot));
+    marker.colors.push_back(cmap(to_robot));
   }
 
   return marker;
 }
 
 std::vector<Marker> build_heading_markers(const KeyedPoses& keyed_poses,
-                                          const VisualizerEdges& odom_edges,
-                                          const std::string& frame_id) {
+                                          const VisualizerEdges& odom_edges) {
   std::vector<Marker> arrows;
 
   int id = 0;
   for (size_t ii = 0; ii < odom_edges.size(); ++ii) {
     Marker marker;
-    marker.header.frame_id = frame_id;
     marker.ns = "heading";
     marker.id = id++;
     marker.action = Marker::ADD;
@@ -170,69 +177,8 @@ std::vector<Marker> build_heading_markers(const KeyedPoses& keyed_poses,
   return arrows;
 }
 
-Marker build_lc_marker(const KeyedPoses& keyed_poses,
-                       const VisualizerEdges& loop_edges,
-                       const std::string& frame_id) {
-  // Publish loop closure edges.
-  Marker marker;
-  marker.header.frame_id = frame_id;
-  marker.ns = "LoopClosures";
-  marker.id = 1;
-  marker.action = Marker::ADD;
-  marker.type = Marker::LINE_LIST;
-  marker.color.r = 0.0;
-  marker.color.g = 0.2;
-  marker.color.b = 1.0;
-  marker.color.a = 0.8;
-  marker.scale.x = 0.02;
-  marker.pose.orientation.w = 1.0;
-
-  for (size_t ii = 0; ii < loop_edges.size(); ++ii) {
-    const auto robot1 = loop_edges[ii].first.first;
-    const auto robot2 = loop_edges[ii].second.first;
-    const auto key1 = loop_edges[ii].first.second;
-    const auto key2 = loop_edges[ii].second.second;
-
-    marker.points.push_back(positionFromKey(keyed_poses, robot1, key1));
-    marker.points.push_back(positionFromKey(keyed_poses, robot2, key2));
-  }
-
-  return marker;
-}
-
-Marker build_rejected_lc_marker(const KeyedPoses& keyed_poses,
-                                const VisualizerEdges& loop_edges,
-                                const std::string& frame_id) {
-  // Publish loop closure edges.
-  // Publish the rejected loop closure edges
-  Marker marker;
-  marker.header.frame_id = frame_id;
-  marker.ns = "RejectedLoopClosures";
-  marker.id = 1;
-  marker.action = Marker::ADD;
-  marker.type = Marker::LINE_LIST;
-  marker.color.r = 0.5;
-  marker.color.g = 0.5;
-  marker.color.b = 0.5;
-  marker.color.a = 0.7;
-  marker.scale.x = 0.02;
-  marker.pose.orientation.w = 1.0;
-
-  for (size_t ii = 0; ii < loop_edges.size(); ++ii) {
-    const auto robot1 = loop_edges[ii].first.first;
-    const auto robot2 = loop_edges[ii].second.first;
-    const auto key1 = loop_edges[ii].first.second;
-    const auto key2 = loop_edges[ii].second.second;
-
-    marker.points.push_back(positionFromKey(keyed_poses, robot1, key1));
-    marker.points.push_back(positionFromKey(keyed_poses, robot2, key2));
-  }
-
-  return marker;
-}
-
 std::vector<Marker> build_node_ids(const KeyedPoses& keyed_poses,
-                                   const std::string& frame_id) {
+                                   double scale) {
   // Publish loop closure edges.
   // Publish node IDs in the pose graph.
 
@@ -241,7 +187,6 @@ std::vector<Marker> build_node_ids(const KeyedPoses& keyed_poses,
   for (const auto& robot : keyed_poses) {
     for (const auto& [key, pose] : robot.second) {
       auto& marker = m_ids.emplace_back();
-      marker.header.frame_id = frame_id;
       marker.ns = "NodeIds";
       marker.action = Marker::ADD;
       marker.type = Marker::TEXT_VIEW_FACING;
@@ -250,7 +195,7 @@ std::vector<Marker> build_node_ids(const KeyedPoses& keyed_poses,
       marker.color.b = 0.2;
       marker.color.a = 0.8;
       // Only Scale z is used - height of capital A in the text
-      marker.scale.z = 0.01;
+      marker.scale.z = scale;
       marker.pose.orientation.w = 1.0;
       marker.pose = pose;
       // Display text for the node
@@ -263,11 +208,9 @@ std::vector<Marker> build_node_ids(const KeyedPoses& keyed_poses,
   return m_ids;
 }
 
-Marker build_keyframes(const KeyedPoses& keyed_poses,
-                       const std::string& frame_id) {
+Marker build_keyframes(const KeyedPoses& keyed_poses, double scale) {
   // Publish keyframe nodes in the pose graph.
   Marker marker;
-  marker.header.frame_id = frame_id;
   marker.ns = "keyframes";
   marker.id = 0;
   marker.action = Marker::ADD;
@@ -276,9 +219,9 @@ Marker build_keyframes(const KeyedPoses& keyed_poses,
   marker.color.g = 1.0;
   marker.color.b = 0.3;
   marker.color.a = 0.8;
-  marker.scale.x = 0.05;
-  marker.scale.y = 0.05;
-  marker.scale.z = 0.05;
+  marker.scale.x = scale;
+  marker.scale.y = scale;
+  marker.scale.z = scale;
   marker.pose.orientation.w = 1.0;
 
   for (const auto& robot : keyed_poses) {
@@ -300,19 +243,40 @@ Visualizer::Visualizer(const rclcpp::NodeOptions& options)
   sub_ = create_subscription<PoseGraph>(
       "graph", 10, [this](const PoseGraph& msg) -> void { callback(msg); });
   pub_ = create_publisher<MarkerArray>("pose_graph_markers", 10);
-  server_ = std::make_shared<interactive_markers::InteractiveMarkerServer>(
-      "interactive_node", this);
+
+  declare_parameter("use_server", false);
+  auto use_server = get_parameter("use_server").as_bool();
+  declare_parameter("show_heading", false);
+  get_parameter("show_heading", show_heading_);
+  declare_parameter("odom_scale", 0.02);
+  get_parameter("odom_scale", odom_scale_);
+  declare_parameter("loop_closure_scale", 0.02);
+  get_parameter("loop_closure_scale", loop_closure_scale_);
+  declare_parameter("rejected_loop_closure_scale", 0.02);
+  get_parameter("rejected_loop_closure_scale", rejected_loop_closure_scale_);
+  declare_parameter("other_edge_scale", 0.02);
+  get_parameter("other_edge_scale", other_edge_scale_);
+  declare_parameter("node_scale", 0.15);
+  get_parameter("node_scale", node_scale_);
+  declare_parameter("text_scale", 0.1);
+  get_parameter("text_scale", text_scale_);
+
+  if (use_server) {
+    server_ = std::make_shared<interactive_markers::InteractiveMarkerServer>(
+        "interactive_node", this);
+  }
 }
 
 void Visualizer::callback(const PoseGraph& msg) {
   // iterate through nodes in pose graph
+  keyed_poses_.clear();
   for (const auto& msg_node : msg.nodes) {
     // Fill pose nodes (representing the robot position)
     keyed_poses_[msg_node.robot_id][msg_node.key] = msg_node.pose;
   }
 
   // update frame id
-  frame_id_ = msg.header.frame_id;
+  last_header_ = msg.header;
 
   odometry_edges_.clear();
   loop_edges_.clear();
@@ -328,6 +292,8 @@ void Visualizer::callback(const PoseGraph& msg) {
       loop_edges_.emplace_back(std::make_pair(from, to));
     } else if (msg_edge.type == PoseGraphEdge::REJECTED_LOOPCLOSE) {
       rejected_loop_edges_.emplace_back(std::make_pair(from, to));
+    } else {
+      other_edges_.emplace_back(std::make_pair(from, to));
     }
   }
 
@@ -339,35 +305,67 @@ void Visualizer::visualize() {
     return;
   }
 
-  MarkerArray ma;
-  // Odometry Edges
-  auto m_odom = build_odom_marker(keyed_poses_, odometry_edges_, frame_id_);
-  ma.markers.push_back(m_odom);
+  std_msgs::msg::ColorRGBA lc_color;
+  lc_color.r = 0.0;
+  lc_color.g = 0.2;
+  lc_color.b = 1.0;
+  lc_color.a = 0.8;
 
-  // Heading (orientation) Edges
-  auto m_headings =
-      build_heading_markers(keyed_poses_, odometry_edges_, frame_id_);
-  ma.markers.insert(ma.markers.end(), m_headings.begin(), m_headings.end());
+  std_msgs::msg::ColorRGBA reject_color;
+  reject_color.r = 0.5;
+  reject_color.g = 0.5;
+  reject_color.b = 0.5;
+  reject_color.a = 0.7;
+
+  MarkerArray msg;
+
+  // Odometry Edges
+  msg.markers.push_back(build_edge_marker(
+      keyed_poses_, odometry_edges_, "odom_edges", &makeColor, odom_scale_));
 
   // Loop Closures
-  auto m_lc = build_lc_marker(keyed_poses_, loop_edges_, frame_id_);
-  ma.markers.push_back(m_lc);
+  msg.markers.push_back(build_edge_marker(
+      keyed_poses_,
+      loop_edges_,
+      "loop_closure_edges",
+      [&](size_t) { return lc_color; },
+      loop_closure_scale_));
 
   // Rejected Loop Closures
-  auto m_rejected_lc =
-      build_rejected_lc_marker(keyed_poses_, rejected_loop_edges_, frame_id_);
-  ma.markers.push_back(m_rejected_lc);
+  msg.markers.push_back(build_edge_marker(
+      keyed_poses_,
+      rejected_loop_edges_,
+      "rejected_loop_closure_edges",
+      [&](size_t) { return reject_color; },
+      rejected_loop_closure_scale_));
+
+  // Any Other Edge
+  msg.markers.push_back(build_edge_marker(keyed_poses_,
+                                          other_edges_,
+                                          "other_edges",
+                                          &makeColor,
+                                          other_edge_scale_));
 
   // Node IDs
-  auto m_node_ids = build_node_ids(keyed_poses_, frame_id_);
-  ma.markers.insert(ma.markers.end(), m_node_ids.begin(), m_node_ids.end());
+  auto m_node_ids = build_node_ids(keyed_poses_, text_scale_);
+  msg.markers.insert(msg.markers.end(), m_node_ids.begin(), m_node_ids.end());
 
   // Keyframes
-  auto m_keyframes = build_keyframes(keyed_poses_, frame_id_);
-  ma.markers.push_back(m_keyframes);
+  auto m_keyframes = build_keyframes(keyed_poses_, node_scale_);
+  msg.markers.push_back(m_keyframes);
+
+  // Heading (orientation) Edges
+  if (show_heading_) {
+    auto m_headings = build_heading_markers(keyed_poses_, odometry_edges_);
+    msg.markers.insert(msg.markers.end(), m_headings.begin(), m_headings.end());
+  }
+
+  for (auto& marker : msg.markers) {
+    marker.header = last_header_;
+  }
 
   // Publish it all!
-  pub_->publish(ma);
+  pub_->publish(msg);
 
   if (!server_) {
     return;
@@ -378,7 +376,7 @@ void Visualizer::visualize() {
     for (const auto& [key, pose] : robot.second) {
       // Display text for the node
       std::string robot_id = std::to_string(key);
-      makeMenuMarker(*server_, frame_id_, pose, robot_id);
+      makeMenuMarker(*server_, last_header_, pose, robot_id);
     }
   }
 
